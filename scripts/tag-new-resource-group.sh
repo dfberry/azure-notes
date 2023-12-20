dd618787dc4bb895bfa88260b60eba91d1fcaae8
Dina Berry (personal) <dfberry@users.noreply.github.com>
2023-12-20 07:38:37 -0800

    Update tag-new-resource-group.sh

--- scripts/tag-new-resource-group.sh
+++ scripts/tag-new-resource-group.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Check if subscription ID is provided
+if [ -z "$1" ]
+then
+    echo "No subscription ID provided. Usage: ./script.sh <subscription_id>"
+    exit 1
+fi
+
+# Set the subscription ID
+subscription_id=$1
+
+# Set the subscription for the session
+az account set --subscription $subscription_id
+
 # Get the current date and time
 current_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
